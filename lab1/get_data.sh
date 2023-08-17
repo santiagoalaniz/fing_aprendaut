c4c23e6e5f5366b6c036eb526688d651cc489eaf
b4-io <brunodesimone17@gmail.com>
2023-08-17 19:46:49 -0300

    santi ortiva

---
+++ lab1/get_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# URL of the ZIP file to download
+zip_url="https://archive.ics.uci.edu/static/public/697/predict+students+dropout+and+academic+success.zip"
+
+# Name of the downloaded ZIP file
+zip_file="data.zip"
+
+# Directory where the ZIP file will be downloaded
+download_dir="."
+
+# Directory where the contents of the ZIP file will be extracted
+extract_dir="./assets"
+
+# Download the ZIP file
+wget "$zip_url" -O "$download_dir/$zip_file"
+
+# Unzip the downloaded ZIP file
+unzip "$download_dir/$zip_file" -d "$extract_dir"
+
+# Remove the downloaded ZIP file
+rm "$download_dir/$zip_file"
+
+echo "ZIP file downloaded, extracted, and deleted."
