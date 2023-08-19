394a575286428a5ddce8d79400c7c02fc7509217
b4-io <brunodesimone17@gmail.com>
2023-08-19 12:35:15 -0300

    predice 60% bien

--- lab1/src/nodo.py
+++ lab1/src/nodo.py
@@ -2,7 +2,7 @@ class Nodo:
     hoja = "hoja"
     rama = "rama"
 
-    def __init__(self, attr, valor, tipo: str = rama, hijos: list['Nodo'] = None, resultado: str = None):
+    def __init__(self, attr, valor, tipo: str = rama, hijos: list['Nodo'] = None, resultado: str = None, n_casos: int = None):
         if tipo not in [Nodo.hoja, Nodo.rama]:
             raise ValueError("Tipo must be 'hoja' or 'rama'")
         
@@ -18,9 +18,15 @@ class Nodo:
         if tipo == Nodo.rama and hijos is None:
             raise ValueError("Hijos must be provided if tipo is 'rama'")
         
+        if n_casos is None or n_casos <= 0:
+            raise ValueError("n_casos must be greater than 0")
+        
         self.attr = attr
         self.valor = valor
         self.tipo = tipo
         self.hijos = hijos
         self.resultado = resultado
+        self.n_casos = n_casos
 
+    def __str__(self):
+        return f"{self.tipo} {self.attr} = {self.valor} -> {self.resultado} ({self.n_casos})"
