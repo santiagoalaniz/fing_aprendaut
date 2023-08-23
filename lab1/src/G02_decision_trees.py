90418b5d36f650af69a9bfd9c08c9ff049295951
Santiago <santiagoalaniz97@gmail.com>
2023-08-23 00:28:34 -0300

    lab01 santiago: entradas agregadas a la seccion algoritmo"

--- lab1/src/G02_decision_trees.py
+++ lab1/src/G02_decision_trees.py
@@ -8,12 +8,13 @@ import src.ID3_utils as utils
 from sklearn.metrics import accuracy_score
 
 class ID3Classifier():
-    def __init__(self, min_samples_split=0, min_split_gain=0.):
+    def __init__(self, min_samples_split=0, min_split_gain=0., attrs_values={}):
         self.min_samples_split = min_samples_split
         self.min_split_gain = min_split_gain
+        self.tree = None
+        self.attrs_values = attrs_values
 
     def fit(self, X, y):
-        self.attributes_values = {attr: X[attr].unique() for attr in self.attributes}
         X['Target'] = y
         self.tree = self.__id3(X, 'Target', X.iloc[:, :-1].columns)
     
@@ -25,31 +26,26 @@ class ID3Classifier():
     
     # private
 
-    ## Mitchell, p. 56
+    ## Mitchell, p. 68
     def __id3(self, exs, attr_tget, attrs):
-        if exs[attr_tget].nunique() == 1: return utils.leaf(1)
-        if exs[attr_tget].nunique() == 0: return utils.leaf(0)
-        if len(attrs) == 0: return utils.leaf(exs[attr_tget].mode()[0])
+        if exs[attr_tget].nunique() == 1: return utils.node(1)
+        if exs[attr_tget].nunique() == 0: return utils.node(0)
+        if len(attrs) == 0: return utils.node(exs[attr_tget].mode()[0])
 
         best_attr, gain = utils.max_gain_attr(exs, attr_tget, attrs)
         
-        if gain <= self.min_split_gain: return utils.leaf(exs[attr_tget].mode()[0])
+        if gain <= self.min_split_gain: return utils.node(exs[attr_tget].mode()[0])
+        
         node = utils.node(best_attr, gain)
-        best_attr_values = self.attributes_values[best_attr]
+        best_attr_values = self.attrs_values[best_attr]
         
         for attr_val in best_attr_values:
             exs_i = exs[exs[best_attr] == attr_val]
 
             if exs_i.shape[0] <= self.min_samples_split: 
-              node.children[attr_val] = utils.leaf(exs[attr_tget].mode()[0])
+              node.children[attr_val] = utils.node(exs[attr_tget].mode()[0])
             else: 
               node.children[attr_val] = self.__id3(exs_i, attr_tget, attrs.drop(best_attr))
         
         return node
         
-
-def main():
-    return 1
-
-if __name__ == "__main__":
-    main()
--- lab1/src/ID3_utils.py
+++ lab1/src/ID3_utils.py
@@ -2,11 +2,15 @@
 # para nuestra implementacion del algoritmo ID3
 
 class ID3Node:
-    def __init__(self, label, children, info_gain):
+    def __init__(self, label, info_gain):
         self.label = label
         self.info_gain = info_gain
-        self.children = children
+        self.children = {}
 
-def pos_root(): return ID3Node("root", True)
+def node(label, info_gain= 1.): return ID3Node(label, info_gain)
 
-def neg_root(): return ID3Node("root", False)
+def max_gain_attr(exs, attr_tget, attrs):
+    return attrs[0], 0.
+
+def evaluate(X, tree):
+    return X.apply(lambda x: 0, axis=1)
