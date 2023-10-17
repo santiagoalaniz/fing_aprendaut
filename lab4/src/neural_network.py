9505a80b0580436a6f595a84ff48ada88b19284a
Maria <maria.usuca@gmail.com>
2023-10-17 17:37:36 -0300

    parte ai,ii

--- lab4/src/neural_network.py
+++ lab4/src/neural_network.py
@@ -19,6 +19,7 @@ class NeuralNetwork(nn.Module):
 
 def train_model(dataloader, model, loss_fn, optimizer,device):
     size = len(dataloader.dataset)
+    total_loss = 0
     model.train()
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
@@ -26,15 +27,16 @@ def train_model(dataloader, model, loss_fn, optimizer,device):
         # Compute prediction error
         pred = model(X)
         loss = loss_fn(pred, y)
+        total_loss += loss.item()
 
         # Backpropagation
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    
+    avg_loss = total_loss / size
+    return avg_loss
+   
 
 def test_model(dataloader, model, loss_fn, device):
     size = len(dataloader.dataset)
@@ -47,6 +49,6 @@ def test_model(dataloader, model, loss_fn, device):
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    avg_loss = test_loss / num_batches
+    acurracy = 100 * correct / size
+    return avg_loss, acurracy
