15608327234229eb86f913065ccc069d28f9199b
Maria <maria.usuca@gmail.com>
2023-10-17 12:56:59 -0300

    wip

--- lab4/src/neural_network.py
+++ lab4/src/neural_network.py
@@ -1,5 +1,6 @@
-from torch import nn
 import torch.nn.functional as F
+import torch
+from torch import nn
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -16,4 +17,36 @@ class NeuralNetwork(nn.Module):
         x = F.softmax(x, dim=1)  # Convertir la salida en una distribución de probabilidad
         return x
 
-  
+def train_model(dataloader, model, loss_fn, optimizer,device):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test_model(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
